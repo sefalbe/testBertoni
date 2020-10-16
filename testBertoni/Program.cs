48cf87d601fba4d34667ff1d0994d0069c681063
Alberto <aserranof@cinepolis.com>
2020-10-15 22:39:50 -0500

    [add] Added project

---
+++ testBertoni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testBertoni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
